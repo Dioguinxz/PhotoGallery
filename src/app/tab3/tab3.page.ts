994335992fd58eb355bac4fb533d650cad4aaf0c
Dioguinxz <doassis@hotmail.com.br>
2024-07-09 21:02:20 -0300

    Fotos Funcionando

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ViewDidEnter } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit, ViewDidEnter {
 
   constructor() {}
+
+  ngOnInit(): void {
+    console.log("Tab 3 built")
+  }
+
+  ionViewDidEnter(): void {
+    console.info("Navegou Tab 3")
+  }
+
+  
 
 }
